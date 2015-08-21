edcdf24e95d355fae5d3a210dfbd524f20064553
Larry Zhang <lzhang124@gmail.com>
2015-08-21 14:01:58 -0600

    scroll change color

--- js/main.js
+++ js/main.js
@@ -10,16 +10,30 @@ $(document).ready(function() {
 		}, 300);
 	});
 
-	$("#down").click(function() {
+	$('#down').click(function() {
 		$('html, body').animate({
-			scrollTop: $("#who").offset().top
+			scrollTop: $('#who').offset().top
 		}, 300);
 	});
 
-	$(".person").hover(function(){
-		$(this).find(".overlay").fadeIn('fast');
+	$('.person').hover(function(){
+		$(this).find('.overlay').fadeIn('fast');
 	},
 	function(){
-		$(this).find(".overlay").fadeOut('fast');
+		$(this).find('.overlay').fadeOut('fast');
+	});
+
+	var tStart = 0
+		, tEnd = 4*$(window).height()+70
+		, cStart = [29,29,29]
+		, cEnd = [32,78,160]
+		, cDiff = [cEnd[0] - cStart[0], cEnd[1] - cStart[1], cEnd[2] - cStart[2]];
+
+	$(document).scroll(function() {
+		var p = ($(this).scrollTop() - tStart) / (tEnd - tStart);
+		p = Math.min(1, Math.max(0, p));
+		var cBg = [Math.round(cStart[0] + cDiff[0] * p), Math.round(cStart[1] + cDiff[1] * p), Math.round(cStart[2] + cDiff[2] * p)];
+		$('body').css('background-color', 'rgb(' + cBg.join(',') +')');
+		$('nav.navbar.navbar-inverse').css('background-color', 'rgb(' + cBg.join(',') +')');
 	});
 });
