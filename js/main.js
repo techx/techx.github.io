ee5323dc1b07681da300022780e671098e94c4dc
Larry Zhang <lzhang124@gmail.com>
2015-08-21 14:05:10 -0600

    don't change navbar color

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(document).ready(function() {
 	});
 
 	var startPos = 0
-		, endPos = 4*$(window).height()+70
+		, endPos = 4*$(window).height()
 		, startColor = [29,29,29]
 		, endColor = [32,78,160]
 		, diffColor = [endColor[0] - startColor[0], endColor[1] - startColor[1], endColor[2] - startColor[2]];
@@ -34,6 +34,6 @@ $(document).ready(function() {
 		pos = Math.min(1, Math.max(0, pos));
 		var bgColor = [Math.round(startColor[0] + diffColor[0] * pos), Math.round(startColor[1] + diffColor[1] * pos), Math.round(startColor[2] + diffColor[2] * pos)];
 		$('body').css('background-color', 'rgb(' + bgColor.join(',') +')');
-		$('nav.navbar.navbar-inverse').css('background-color', 'rgb(' + bgColor.join(',') +')');
+		// $('nav.navbar.navbar-inverse').css('background-color', 'rgb(' + bgColor.join(',') +')');
 	});
 });
